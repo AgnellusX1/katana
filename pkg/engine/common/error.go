10f97c95538e676f70210b38d7cab203a26214af
Doğan Can Bakır <dogancanbakir@protonmail.com>
2026-04-03 18:32:00 +0530

    Add per-domain page budget option

--- pkg/engine/common/error.go
+++ pkg/engine/common/error.go
@@ -4,3 +4,4 @@ import "errors"
 
 var ErrOutOfScope = errors.New("out of scope")
 var ErrMaxDepthReached = errors.New("max depth reached")
+var ErrDomainPageBudgetExhausted = errors.New("domain page budget exhausted")
